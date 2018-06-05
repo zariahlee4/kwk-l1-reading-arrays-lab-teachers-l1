8144750c289e19ca11ee5ad7809e23c126c12cb4
zariahlee4 <sneakiana@gmail.com>
2018-06-05 22:08:15 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,10 +9,13 @@ STUDENT_NAMES = [
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES [0]
+  
 end
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
+  student_names
 end
 
 def last_student_by_index
